6fe4903544bdb85c6c7e72c0d6d54e00038b56d2
Abhishek Agrawal <abhishek.agrawal@allianz.de>
2020-02-21 02:56:59 +0100

    Organize imports

--- src/main/java/com/org/hse/entity/Product.java
+++ src/main/java/com/org/hse/entity/Product.java
@@ -1,12 +1,12 @@
 package com.org.hse.entity;
 
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
